198147be9e64b3120e05a9e7a0a138f43e44054b
Kaustav-CS <69429716+Kaustav-CS@users.noreply.github.com>
2021-09-15 00:37:37 +0530

    #Codefoces_1566A[A. Median Maximization]
    A. Median Maximization
    time limit per test: 1 second
    memory limit per test: 256 megabytes
    input: standard input
    output: standard output
    
    You are given two positive integers and . Find the maximum possible median of an array of non-negative integers (not necessarily distinct), such that the sum of its elements is equal to .
    A median of an array of integers of length m is the number standing on the[m/2] -th (roundingup) position in the non-decreasing ordering of its elements. Positions are numbered starting from 1. For example, a median of the array [20, 40, 20, 50, 50, 30] is the [m/2]-th element of [20, 20, 30, 40, 50, 50], so it is 30 . There exist other definitions of the median, but in this problem we use the described definition.
    
    Input
    The input consists of multiple test cases. The first line contains a single integer t.
    ( 1<= t <= 10^4 )— the number of test cases. Description of the test cases follows.
    Each test case contains a single line with two integers  'n' and 's'( ) — the length
    of the array and the required sum of the elements.
    Output
    For each test case print a single integer — the maximum possible median.

---
+++ Codefoces_1566A_Median_Maximization.py
@@ -0,0 +1,51 @@
+#Codefoces_1566A
+k2 = []
+def code():
+
+    n,s = map(int, input().split())
+    #print("\t\t",n,s)
+    #n,s = int(input()).split()#,int(input())
+    #s = int(input())
+    if(n%2 == 1):
+        k = (n//2) +1
+    else:
+        k = n//2
+    q = n-k+1
+    z = (s//q)
+    k2.append(z)
+    #print(z)
+
+
+t = int(input())
+while(t!=0):
+    t = t-1
+    code()
+
+print("\n")
+print("\n")
+print("\n")
+for j in range(len(k2)):
+    print(k2[j])
+
+##
+##'''
+##        Input
+##8
+##1 5
+##2 5
+##3 5
+##2 1
+##7 17
+##4 14
+##1 1000000000
+##1000000000 1
+##        Output
+##5
+##2
+##2
+##0
+##4
+##4
+##1000000000
+##0
+##'''
