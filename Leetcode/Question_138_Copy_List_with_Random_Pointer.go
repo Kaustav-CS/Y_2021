2645cecebdc90af6ffa2851a02a9ca1cb7e737fe
Kaustav-CS <69429716+Kaustav-CS@users.noreply.github.com>
2021-11-19 09:28:33 +0530

    Question_138_Copy_List_with_Random_Pointer
    /******************************************************************************
    A linked list of length 'n' is given such that each node contains an additional
    random pointer, which could point to any node in the list, or 'null'.
    
    Construct a 'deep copy' of the list. The deep copy should consist of exactly
    'n' "brand new" nodes, where each new node has its value set to the value of
    its corresponding original node. Both the 'next' and 'random' pointer of the
    new nodes should point to new nodes in the copied list such that the pointers
    in the original list and copied list represent the same list state.
    "None of the pointers in the new list should point to nodes in the
    original list".
    
    For example, if there are two nodes 'X' and 'Y' in the original list,
    where 'X.random --> Y', then for the corresponding two nodes 'x' and 'y' in
    the copied list, 'x.random --> y'.
    
    Return the head of the copied linked list.
    
    The linked list is represented in the input/output as a list of n nodes. Each
    node is represented as a pair of '[val, random_index]' where:
    
        'val': an integer representing 'Node.val'
        'random_index': the index of the node (range from '0' to 'n-1') that the
        'random' pointer points to, or 'null' if it does not point to any node.
    
    Your code will only be given the 'head' of the original linked list.
    
     
    
    Example 1:
    
    Input: head = [[7,null],[13,0],[11,4],[10,2],[1,0]]
    Output: [[7,null],[13,0],[11,4],[10,2],[1,0]]
    
    Example 2:
    
    Input: head = [[1,1],[2,1]]
    Output: [[1,1],[2,1]]
    
    Example 3:
    
    Input: head = [[3,null],[3,0],[3,null]]
    Output: [[3,null],[3,0],[3,null]]
    
    Example 4:
    
    Input: head = []
    Output: []
    Explanation: The given linked list is empty (null pointer), so return null.
    
    
    
    *******************************************************************************/

--- Leetcode/Question_138_Copy_List_with_Random_Pointer.go
+++ Leetcode/Question_138_Copy_List_with_Random_Pointer.go
@@ -3,24 +3,49 @@ Link:   https://leetcode.com/problems/copy-list-with-random-pointer/
 138. Copy List with Random Pointer
 
 Your input
-[2,2,3,2]
+[[7,null],[13,0],[11,4],[10,2],[1,0]]
 
 Output
-3
+[[7,null],[13,0],[11,4],[10,2],[1,0]]
 
 Expected
-3
+[[7,null],[13,0],[11,4],[10,2],[1,0]]
 
-DATE:    2021, November 18
-০২ অগ্রহায়ণ,১৪২৮
+DATE:    2021, November 19
+০৩ অগ্রহায়ণ,১৪২৮
 
 *******************************************************************************/
-func singleNumber(nums []int) int {
-    a, b := 0, 0
-    for _, num := range nums {
-        a = a ^ num & ^b
-        b = b ^ num & ^a
-    }
-    return a | b
-}
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Next *Node
+ *     Random *Node
+ * }
+ */
 
+func copyRandomList(head *Node) *Node {
+    m := make(map[*Node]*Node)
+    
+    dummy := &Node{}
+    
+    curr1 := head
+    curr2 := dummy
+    for curr1 != nil {
+        newNode := &Node{Val: curr1.Val}
+        m[curr1] = newNode
+        curr2.Next = newNode
+        curr1 = curr1.Next
+        curr2 = curr2.Next
+    }
+    
+    curr1 = head
+    curr2 = dummy.Next
+    for curr1 != nil {
+        curr2.Random = m[curr1.Random]
+        curr1 = curr1.Next
+        curr2 = curr2.Next
+    }
+    
+    return dummy.Next
+}
